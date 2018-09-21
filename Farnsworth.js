0039fc40721b0bd90b47798648dae32ac19de9a6
Tim <tgutmann87@gmail.com>
2018-09-21 10:18:43 -0700

    Laptop: Main file changes

--- Farnsworth.js
+++ Farnsworth.js
@@ -4,8 +4,7 @@ const LFG = require("./lfg");
 const Roles = require("./roles");
 const client = new Discord.Client();
 
-var lfgTimer; //setTimeout variable
-var lfgMembers = []; //Array of User id
+var commandToken = "!";
 
 client.on("ready", () => {
   console.log("I am ready!");
@@ -15,17 +14,18 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
 	var commands = message.content.split(' ');
+	commands[0] = commands[0].toLowerCase();
 	
 	//!ping: Basic test ping command
-	if (message.content.startsWith("!ping")) {
+	if (commands[0] == commandToken + "ping") {
 		Ping.ping(message);
 	}
 	//!Roll <integer>: Rolls a x sided die
-	else if(message.content.startsWith("!Roll")) {
-		message.channel.send(Math.floor((Math.random() * parseInt(commands[1]) + 1)));
+	else if(commands[0] == commandToken + "roll") {
+		message.channel.send("<@" + message.author.id + "> Rolled a: " + Math.floor((Math.random() * parseInt(commands[1]) + 1)));
 	}
 	//!WhoAmI: Displays some user account information
-	else if(message.content.startsWith("!WhoAmI"))
+	else if(commands[0] == commandToken + "whoami")
 	{
 		message.channel.send("User: " + message.author.username);
 		message.channel.send("User ID: " + message.author.id);
@@ -35,22 +35,22 @@ client.on("message", (message) => {
 		message.channel.send("Tag: " + message.author.tag);
 	}
 	//!LFG <integer>: Starts looking for group with max of x people
-	else if(message.content.startsWith("!LFG"))
+	else if(commands[0] == commandToken + "lfg")
 	{	
 		LFG.Start(message, client);
 	}
 	//!JoinUp Allows user to join current LFG
-	else if(message.content.startsWith("!JoinUp"))
+	else if(commands[0] == commandToken + "joinup")
 	{
 		LFG.Join(message, client);
 	}
 	//!Subscribe Allows user to join game based roles
-	else if(message.content.startsWith("!Subscribe"))
+	else if(commands[0] == commandToken + "subscribe")
 	{
 		Roles.Subscribe(message, client, commands[1]);
 	}
 	//!Unsubscribe Allows user to leave game based roles
-	else if(message.content.startsWith("!Unsubscribe"))
+	else if(commands[0] == commandToken + "unsubscribe")
 	{
 		Roles.Unsubscribe(message, client, commands[1]);
 	}
